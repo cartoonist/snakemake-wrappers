8c2379545b98913afda6fa577606711ac0df8faa
Ali Ghaffaari <ali.ghaffaari@hhu.de>
2021-02-07 01:34:08 +0100

    Fix bio/vg/merge wrapper
    Merging "vg" files cannot be merged by simply concatenating them anymore because of
    recently introduced headers in the file format. `vg combine` does the job instead.

--- bio/vg/merge/wrapper.py
+++ bio/vg/merge/wrapper.py
@@ -6,11 +6,9 @@ __license__ = "MIT"
 
 from snakemake.shell import shell
 
-log = snakemake.log_fmt_shell(stdout=False)
+log = snakemake.log_fmt_shell()
 
 shell(
     "(vg ids --join {snakemake.input.vgs} &&"
-    " for VGFILE in {snakemake.input.vgs};"
-    " do cat $VGFILE >> {snakemake.output.merged};"
-    " done) {log}"
+    " vg combine {snakemake.input.vgs} > {snakemake.output.merged}) {log}"
 )
